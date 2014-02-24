f04e014974acee740c5674fef0b93114a38acfa8
Andreas Dewes <andreas.dewes@gmail.com>
2014-02-24 12:37:47 -0500

    Fixed conf.py to allow using RTD template locally.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -264,8 +264,10 @@ texinfo_documents = [
 
 #ReadTheDocs Sphinx Template
 
-import sphinx_rtd_theme
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
