ebbc07a1d5cd79f64c6dc0c636cb3e371f742be6
Andreas Dewes <andreas.dewes@gmail.com>
2015-10-07 13:11:12 +0200

    Removed support for arbitrary arguments.

--- blitzdb/fields/base.py
+++ blitzdb/fields/base.py
@@ -1,10 +1,10 @@
 
 class BaseField(object):
     
-    def __init__(self,key = None,nullable = True,unique = False,indexed = False,**kwargs):
+    def __init__(self,key = None,nullable = True,unique = False,default = None,indexed = False):
         self.key = key
         self.nullable = nullable
         self.indexed = indexed
-        self.opts = kwargs
+        self.default = default
         self.unique = unique
 
