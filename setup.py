067b32ea9252835c952bc51b87d74286610211a6
Andreas Dewes <andreas.dewes@dcso.de>
2017-01-16 12:09:24 +0100

    Do not coerce server_default values.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='blitzdb',
-    version='0.3.3',
+    version='0.4.4',
     author='Andreas Dewes - 7scientists',
     author_email='andreas@7scientists.com',
     license='MIT',
@@ -45,6 +45,8 @@ If you should encounter any problems when using BlitzDB, please feel free to `su
 Changelog
 =========
 
+* 0.4.4: SQL backend: Do not coerce server_default values via a CAST, as this can cause incompatibilities.
+* 0.4.3: Many small improvements to the SQL backend.
 * 0.3.3: Be more robust against corrupted data for foreign key objects. Fix problem with intersection of query sets in SQL backend.
 * 0.3.2: Give the user more control over cascading deletes for foreign keys and M2M objects in the SQL backend.
 * 0.3.1: Made intersect statement more efficient (and avoid using CTEs for that).
